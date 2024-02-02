cc0b60640100a7a4edd46689d580260e520f9acf
Seungchan Kim <122209935+seungchan62@users.noreply.github.com>
2024-02-02 22:54:28 +0900

    Update JumpKingMario.py

--- JumpKingMario.py
+++ JumpKingMario.py
@@ -2,12 +2,13 @@ import pygame
 import sys
 import random
 
-pygame.mixer.init()  # 믹서 모듈 초기화
-jump_sound = pygame.mixer.Sound('jumping.mp3')  # 점프 사운드 로드
-background_sound = pygame.mixer.Sound('marioMusic.mp3')  # 배경 음악 로드
-game_over_sound = pygame.mixer.Sound('gameOverMusic.mp3')  # 게임 오버 사운드 로드
-background_sound.play(-1)  # 배경 음악 반복 재생
+pygame.mixer.init()
+jump_sound = pygame.mixer.Sound('jumping.mp3')  # 점프 효과음
+background_sound = pygame.mixer.Sound('marioMusic.mp3')  # 배경 음악
+game_over_sound = pygame.mixer.Sound('gameOverMusic.mp3')  # 게임 오버 음악
+background_sound.play(-1)
 
+# 마리오 클래스 정의
 class Mario:
     def __init__(self):
         self.x = 100
@@ -15,12 +16,12 @@ class Mario:
         self.jump = 0
         self.gravity = 5
         self.jumpspeed = 5
-        image = pygame.image.load('superMario.png')  # 이미지 로드
-        self.image = pygame.transform.scale(image, (30, 50))  # 이미지 크기 조정
+        image = pygame.image.load('superMario.png')
+        self.image = pygame.transform.scale(image, (30, 50))
         self.invincible = False
         self.blink = False
-        self.on_ground_or_sky = False  # add this line in your __init__ function
-        self.continues = 0  # Number of continues used
+        self.on_ground_or_sky = False
+        self.continues = 0
 
     def update(self):
         self.y += self.gravity
@@ -29,8 +30,7 @@ class Mario:
             self.gravity = -self.jumpspeed
             self.jump -= 1
 
-        # Update on_ground_or_sky based on Mario's y position
-        if self.y <= 0 or self.y >= 600:  # replace with the dimensions of your ground and sky
+        if self.y <= 0 or self.y >= 600:
             self.on_ground_or_sky = True
         else:
             self.on_ground_or_sky = False
@@ -39,7 +39,7 @@ class Mario:
         if not self.blink:
             window.blit(self.image, (self.x, self.y))
 
-
+# 파이프 클래스 정의
 class Pipe:
     def __init__(self, current_speed):
         self.x = 400
@@ -49,35 +49,14 @@ class Pipe:
         self.speed = current_speed
         self.passed = False
 
-
     def update(self):
         self.x -= self.speed
 
     def draw(self, window):
         pygame.draw.rect(window, (0, 255, 0), (self.x, self.y, self.width, self.gap))
         pygame.draw.rect(window, (0, 255, 0), (self.x, self.y + self.gap + 200, self.width, 500))
-#파이프 이미지 추가ver
-# class Pipe:
-#     def __init__(self, current_speed):
-#         self.x = 400
-#         self.y = 0
-#         self.gap = random.randint(100, 300)
-#         self.width = 100
-#         self.speed = current_speed
-#         self.passed = False
-#         image_upper = pygame.image.load('pipeUpper.png')  # load upper pipe image
-#         image_lower = pygame.image.load('pipeLower.png')  # load lower pipe image
-#         self.image_upper = pygame.transform.scale(image_upper, (self.width, 500))  # scale the upper pipe image
-#         self.image_lower = pygame.transform.scale(image_lower, (self.width, 500))  # scale the lower pipe image
-#
-#     def update(self):
-#         self.x -= self.speed
-#
-#     def draw(self, window):
-#         window.blit(self.image_upper, (self.x, self.y))
-#         window.blit(self.image_lower, (self.x, self.y + self.gap + 200))
 
-
+# 점수 클래스 정의
 class Score:
     def __init__(self):
         self.current_score = 0
@@ -104,10 +83,10 @@ class Score:
         best_score_text = self.font.render(f"Best: {self.best_score}", True, (255, 0, 0))
         window.blit(best_score_text, (window.get_width() - best_score_text.get_width() - 10, 10))
 
-
+# 충돌 체크 함수
 def collision(mario, pipes):
     if mario.invincible:
-        return False  # 무적 모드인 경우 바로 False 반환
+        return False
     mario_rect = pygame.Rect(mario.x, mario.y, 30, 50)
 
     for pipe in pipes:
@@ -118,6 +97,7 @@ def collision(mario, pipes):
             return True
     return False
 
+# 게임 오버 화면 함수
 def game_over(score, current_speed, game_time):
     global mario
     background_sound.stop()
@@ -127,7 +107,6 @@ def game_over(score, current_speed, game_time):
     restart = font.render("Press R to replay", True, (255, 255, 255))
     cont = font.render("Press C to continue", True, (255, 255, 255))
 
-    # Calculate the positions to center the text
     text_x = (window.get_width() - text.get_width()) // 2
     scoreText_x = (window.get_width() - scoreText.get_width()) // 2
     restart_x = (window.get_width() - restart.get_width()) // 2
@@ -143,46 +122,43 @@ def game_over(score, current_speed, game_time):
     window.blit(cont, (cont_x, cont_y))
     pygame.display.update()
 
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                sys. exit()
+                sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_r:
-                    mario = Mario()  # Reset Mario, which also resets the continues count
+                    mario = Mario()
                     mario.blink = False
                     game_over_sound.stop()
                     background_sound.play(-1)
-                    score.previous_speed = 2 # reset game speed
-                    score.previous_time = 0 # reset game time
-                    score.reset_score() # reset score
-                    return False, 2, 0 # Reset game speed and time, then restart the game
+                    score.previous_speed = 2
+                    score.previous_time = 0
+                    score.reset_score()
+                    return False, 2, 0
                 elif event.key == pygame.K_c and not mario.on_ground_or_sky:
-                    if mario.continues < 2:  # Allow continue if it has been used less than 2 times
+                    if mario.continues < 2:
                         game_over_sound.stop()
                         background_sound.play(-1)
-                        score.previous_speed = current_speed  # Save current speed
-                        score.previous_time = game_time  # Save game time
-                        mario.invincible = True  # Set invincible mode
-                        mario.continues += 1  # Increment the count of continues used
-                        pygame.time.set_timer(pygame.USEREVENT,
-                                              2000)  # Set a timer to disable invincibility after 1 second
-                        pygame.time.set_timer(pygame.USEREVENT + 1,
-                                              70)  # Set a timer to toggle blinking every 0.1 seconds
-                        return True, current_speed, game_time  # Play the game at current game speed and time
+                        score.previous_speed = current_speed
+                        score.previous_time = game_time
+                        mario.invincible = True
+                        mario.continues += 1
+                        pygame.time.set_timer(pygame.USEREVENT, 2000)
+                        pygame.time.set_timer(pygame.USEREVENT + 1, 70)
+                        return True, current_speed, game_time
                     else:
                         print("You have reached the maximum number of continues. Press R to restart.")
 
-
-# pygame 초기화
+# 초기화 및 게임 창 설정
 pygame.init()
 window = pygame.display.set_mode((400, 600))
 font = pygame.font.Font(None, 36)
 timer_font = pygame.font.Font(None, 24)
-score = Score()  # Score 객체 생성
+score = Score()
 
+# 게임 메인 루프
 def game():
     global mario, last_speed_increase, current_speed, pipes
     mario = Mario()
@@ -192,31 +168,31 @@ def game():
     pipes = [Pipe(current_speed)]
     clock = pygame.time.Clock()
     isRunning = True
-    MAX_SPEED_INCREASE_TIME = 120  # 2 minutes in seconds
+    MAX_SPEED_INCREASE_TIME = 120
 
     while isRunning:
-        game_time = (pygame.time.get_ticks() - game_start_time) // 1000  # 게임 시간 계산
+        game_time = (pygame.time.get_ticks() - game_start_time) // 1000
 
         if game_time <= MAX_SPEED_INCREASE_TIME:
             if game_time - last_speed_increase >= 5:
                 current_speed += 0.1
                 last_speed_increase = game_time
         else:
-            current_speed = 4.4  # Set the speed to a fixed value after 2 minutes
+            current_speed = 4.4
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.USEREVENT:  # 무적 모드 해제 이벤트
+            elif event.type == pygame.USEREVENT:
                 mario.invincible = False
                 mario.blink = False
-                pygame.time.set_timer(pygame.USEREVENT, 0)  # 타이머 취소
-            elif event.type == pygame.USEREVENT + 1:  # 깜빡임 토글 이벤트
+                pygame.time.set_timer(pygame.USEREVENT, 0)
+            elif event.type == pygame.USEREVENT + 1:
                 if mario.invincible:
                     mario.blink = not mario.blink
                 else:
-                    pygame.time.set_timer(pygame.USEREVENT + 1, 0)  # Cancel timer if not invincible
+                    pygame.time.set_timer(pygame.USEREVENT + 1, 0)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     jump_sound.play()
@@ -252,5 +228,6 @@ def game():
         pygame.display.update()
         clock.tick(90)
 
+# 게임 루프 실행
 while True:
     game()
